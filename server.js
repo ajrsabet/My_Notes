ba69baf8b0102d21215b9d4303d290421fd83751
Arauco <arishel@gmail.com>
2020-01-08 16:07:46 -0800

    copy all provided files into repo, start server.js

---
+++ server.js
@@ -0,0 +1,53 @@
+let express = require("express");
+let path = require("path");
+
+let app = express();
+
+//In order to make sure your server works on local & heroku you have to make sure the commandline looks like this
+//process.env.PORT=heroku's port or local port
+const PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+
+//customer DATA
+let notesData = [{
+      title: "",
+      text: "",
+      id: 1,
+    }
+];
+
+
+//Routing
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "notes.html"));
+})
+
+// Displays all notes data
+app.get("/api/notes", function (req, res) {
+    return res.json(notesData);
+});
+
+
+// Create New Characters - takes in JSON input
+app.post("/api/notes", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newNote = req.body;
+    
+        notesData.push(newNote)
+        res.json(true)
+});
+
+
+
+app.listen(PORT, function () {
+    console.log("listenin on port" + PORT);
+})
